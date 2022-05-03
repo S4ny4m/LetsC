40d59d54c1c71e65db35d9011823afde1d8d21d6
Sanyam Pandey <off.sanyam@gmail.com>
2022-05-03 06:41:24 +0530

    commit

---
+++ JoDD_SW/Pointer/AryRevPtr.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*here we will print a series of number in
+reverse order using pointer.*/
+
+int main()
+{
+    int n;
+    printf("Enter the number of Inputs:\n");
+    scanf("%d", &n);
+    int a[n], i;
+    for (i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+    int *ptr = a;
+    for (i = n - 1; i >= 0; i--)
+        printf("%d ", *(ptr + i));
+    return 0;
+}
